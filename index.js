22dab662233c8e9651e56c2d833e3f3f879ae4d2
Durlav <durlavk98@gmail.com>
2023-01-30 16:42:33 +0530

    added testing with supertest and jest + some modification

--- index.js
+++ index.js
@@ -1,97 +1,10 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+const app = require('./app') // the actual Express app
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
+const server = http.createServer(app)
 
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
-
-const Note = require('./models/note')
-
-app.get('/', (request,response) => {
-  response.send('Hello world!')
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = new Note ({
-    content: body.content,
-    important: body.important || false
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
-})
-
-app.get('/api/notes', (request,response) => {
-  Note.find({}).then(notes=>{
-    response.json(notes)
-  })
-})
-
-app.delete('/api/notes/:id', (request,response) => {
-  Note.findByIdAndRemove(request.params.id)
-  .then(result => {
-    response.status(204).end()
-  })
-  .catch(error => next(error))
-})
-
-app.get('/api/notes/:id', (request,response, next) => {
-  Note.findById(request.params.id)
-  .then(note => {
-    if(note){
-      response.json(note)
-    } else {
-      response.status(404).end( )
-    }
-  })
-  .catch(error => {
-    next(error)
-  })
-})
-
-app.put('/api/notes/:id', (request,response) => {
-  const body = request.body
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true, runValidators: true, context: 'query' })
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
-})
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log('Server running on port', PORT);
-})
-
